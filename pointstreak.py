26a888aee1cd55c6a831c3bf4fe8601722d77aeb
Qaiser Shakoor <link2qaiser@gmail.com>
2020-05-01 02:09:16 +0500

    ADD CODE TO SCRAP FROM START

--- pointstreak.py
+++ pointstreak.py
@@ -2,10 +2,10 @@ from selenium import webdriver
 driver = webdriver.Chrome()
 
 
-def getPoints(url):
+def getPoints():
     global driver
 
-    driver.get(url)
+    #driver.get(url)
     xpathTd = '//*[@id="ps-content-bootstrapper"]/div/div/div[2]/div[3]/div/div/table/tbody/tr'
 
     players = driver.find_elements_by_xpath(xpathTd)
@@ -28,7 +28,7 @@ def getPoints(url):
     print(grandArr)
 
 
-url = 'http://baseball.pointstreak.com/team_roster.html?teamid=65683&seasonid=32546&sortby=jersey'
+#url = 'http://baseball.pointstreak.com/team_roster.html?teamid=65683&seasonid=32546&sortby=jersey'
 
 #getPoints(url)
 
@@ -36,3 +36,22 @@ url = 'http://baseball.pointstreak.com/team_roster.html?teamid=65683&seasonid=32
 mainUrl = 'http://baseball.pointstreak.com/'
 
 driver.get(mainUrl)
+
+colls = driver.find_elements_by_xpath(
+    '//*[@id="featured-client"]/div/ul[2]/li/a')
+
+#Colls Link
+for coll in colls:
+    collLink = coll.get_attribute("href")
+    driver.get(collLink)
+    #League Links
+    leags = driver.find_elements_by_xpath(
+        '//*[@id="ps-content-bootstrapper"]/div/div/div[2]/div[4]/div/div/table/tbody/tr/td[1]/a'
+    )
+    for leag in leags:
+        leagLink = leag.get_attribute("href")
+        driver.get(leagLink)
+        driver.find_element_by_xpath(
+            '/html/body/div[3]/div[1]/div/div/div[1]/div/div[2]/ul[1]/li[7]/a'
+        ).click()
+        getPoints()
